1a5669ec4658ca13c915adb21c4e98373495ec90
tow02 <tow02@Paweens-MacBook-Pro.local>
2015-08-24 16:43:38 +0700

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Asturias"
+  end
+
   test "should get home" do
   get :home
   assert_response :success
-  assert_select "title", "Home | MUIC Music Club Room Reservation System"
+  assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
   get :help
   assert_response :success
-  assert_select "title", "Help | MUIC Music Club Room Reservation System"
+  assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | MUIC Music Club Room Reservation System"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
